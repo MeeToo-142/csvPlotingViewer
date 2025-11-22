6f2b33445b7af05237fcee3a684edda77f4474cb
Da Hwa <john.213steven@gmail.com>
2025-11-22 20:21:49 +0700

    for deploy

--- CsvViewer/src/components/navbar/NavBar.tsx
+++ CsvViewer/src/components/navbar/NavBar.tsx
@@ -1,4 +1,3 @@
-import { PageRoutes } from "../../Constants"
 import "../../index.css"
 import "./NavBar.css"
 
@@ -6,7 +5,7 @@ function NavBar() {
   return (
     <nav>
         <div className="navRight">
-          <a href={PageRoutes.homepage.path} className="logo">@csvViewer</a>
+          <a href={`${import.meta.env.BASE_URL}`} className="logo">@csvViewer</a>
         </div>
         <div className="navLeft">
           <a href="https://github.com/John-Da/csvPlotter-ReactFlask" target="_blank" className="linkto">GitHub</a>
